b0780f18ab184ae369e7cd1741a8a57e861f1cf0
ramaherdianto <ramadhanherdianto35@gmail.com>
2023-01-20 18:53:33 +0700

    feat: create context for cosuming API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.scss';
+import MainContent from './components/MainContent';
 
 function App() {
-    return <div className='App min-h-screen'></div>;
+    return (
+        <div className='App min-h-screen'>
+            <MainContent />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useContext } from 'react';
+import { MyContext } from './Contex';
+
+const Home = () => {
+    return (
+        <>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi alias beatae labore
+                amet quos rerum possimus non facere obcaecati fugit?
+            </p>
+        </>
+    );
+};
+
+export default Home;
