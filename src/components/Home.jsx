8c5890ea30de5aee1f10b803ce7cd232e8bdc377
ramaherdianto <ramadhanherdianto35@gmail.com>
2023-01-22 17:39:18 +0700

    feat: create banners

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useContext } from 'react';
+import Banner from './Banner';
 import { MyContext } from './Context';
 import Navbar from './Navbar';
 
@@ -7,6 +8,9 @@ const Home = () => {
     return (
         <>
             <Navbar />
+            <header className='max-w-7xl mx-auto mt-20 px-4 sm:px-4 md:px-4 lg:px-4 xl:px-0 2xl:px-0'>
+                <Banner />
+            </header>
         </>
     );
 };
