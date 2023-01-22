f64279f33108fad1cfb05c13df091b5f5ee0a8b5
ramaherdianto <ramadhanherdianto35@gmail.com>
2023-01-22 13:54:46 +0700

    feat: custom breakpoint screens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,13 @@ module.exports = {
             dark: '#2C2C2C',
             grey: '#9C9C9C',
         },
+        screens: {
+            sm: { min: '640px', max: '767px' },
+            md: { min: '768px', max: '1023px' },
+            lg: { min: '1024px', max: '1279px' },
+            xl: { min: '1280px', max: '1535px' },
+            '2xl': { min: '1536px' },
+        },
     },
     plugins: [],
 };
